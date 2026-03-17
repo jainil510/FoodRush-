b4f1ef80cbf126f9e9a9942b36b43a67778946a4
Jainil <jainilpatil510@gmail.com>
2026-03-17 12:18:00 +0530

    Add all application components and fix JWT properties

---
+++ src/main/java/com/foodrush/food_ordering_system/config/JwtProperties.java
@@ -0,0 +1,8 @@
+package com.foodrush.food_ordering_system.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties
+public class JwtProperties {
+    
+}
